7ce0b0dc5dcfecd715743f1086208e15fa26c046
Kamil <milo.macek@gmail.com>
2019-03-11 16:40:16 +0100

    GUI refactor and added connection to database, properties file

--- src/controller/AdminAddEmployeeController.java
+++ src/controller/AdminAddEmployeeController.java
@@ -5,32 +5,67 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import persistancemanagers.EnumManager;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Calendar;
 import java.util.ResourceBundle;
 
 public class AdminAddEmployeeController implements Initializable {
 
     @FXML private AnchorPane rootPane;
     @FXML private Label labelFirstName;
-    @FXML private Label labelSecondName;
+    @FXML private Label labelLastName;
     @FXML private Label labelDate;
     @FXML private TextField textFieldFirstName;
-    @FXML private TextField textFieldSecondName;
+    @FXML private TextField textFieldLastName;
     @FXML private TextField textFieldLogin;
     @FXML private TextField textFieldPassword;
-    @FXML private ChoiceBox chBoxType;
+    @FXML private ComboBox comboBoxType;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // labelFirstName.setText();
-        //labelSecondName.setText();
-        labelDate.setText(LocalDate.now().toString());
+        updateHeader();
+
+        EnumManager em = new EnumManager();
+        try {
+            em.employeeTypeEnum(comboBoxType,"employee_type");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateHeader() {
+        String time = new SimpleDateFormat("yyyy-MM-dd HH:mm").format(Calendar.getInstance().getTime());
+        labelDate.setText(time);
+    }
+
+    public String getFirstName() {
+        return textFieldFirstName.getText();
+    }
+
+    public String getLastName() {
+        return textFieldLastName.getText();
+    }
+
+    public String getLogin() {
+        return textFieldLogin.getText();
+    }
+
+    public String getPassword() {
+        return textFieldPassword.getText();
+    }
+
+    public String getType() {
+        return comboBoxType.getSelectionModel().getSelectedItem().toString();
     }
 
     public void btnAddEmployeePushed(ActionEvent actionEvent) throws IOException {
